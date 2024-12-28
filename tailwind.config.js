bb0383568a87ad683e5938f2caf7f8e79fc79f61
JoleneKearse <pharaohnutz@gmail.com>
2024-12-28 16:16:07 -0500

    chore: scaffold project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+    colors: {
+      brand: {
+        100: "#bea6a0",
+        200: "#ab9590",
+        300: "#988580",
+        400: "#857470",
+        500: "#726460",
+        600: "#5f5350",
+        700: "#4c4240",
+        800: "#393230",
+        900: "#262120",
+        950: "#131110",
+      },
+      accent: {
+        "red": "#de0808",
+        "violet": "#a22b82",
+        "purple": "#a22b82",
+      },
+    },
+		extend: {},
+	},
+	plugins: [],
+};
