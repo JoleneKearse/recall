2af4b7a18a6bf6c5f80a9a316ef4fc4371441b6a
JoleneKearse <pharaohnutz@gmail.com>
2024-12-30 13:32:40 -0500

    chore: update toggle outline

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -14,7 +14,7 @@ const ModeToggle = () => {
           aria-checked={theme === "dark"}
           aria-label="Toggle light and dark mode"
 				/>
-				<div className="peer ring-2 ring-gray-500 bg-gradient-to-r from-accent-red-200 to-accent-red-300 rounded-full outline-none duration-500 w-20 h-8 shadow-inner peer-checked:bg-gradient-to-r peer-checked:from-accent-red-100 peer-checked:to-accent-red-200 shadow-neutral-800 peer-focus:ring-4 peer-focus:ring-gray-500 relative after:absolute after:w-12 after:h-12 after:top-[-8px] after:left-[-8px] after:bg-klingon after:bg-contain after:bg-no-repeat peer-checked:after:translate-x-14">
+				<div className="peer ring-2 ring-brand-900 dark:ring-accent-red-300 bg-gradient-to-r from-accent-red-200 to-accent-red-300 rounded-full outline-none duration-500 w-20 h-8 shadow-inner peer-checked:bg-gradient-to-r peer-checked:from-accent-red-100 peer-checked:to-accent-red-200 shadow-neutral-800 peer-focus:ring-4 peer-focus:ring-gray-500 relative after:absolute after:w-12 after:h-12 after:top-[-8px] after:left-[-8px] after:bg-klingon after:bg-contain after:bg-no-repeat peer-checked:after:translate-x-14">
 </div>
 
 			</label>
